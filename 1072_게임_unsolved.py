944807a6b9f00aab68882a3d74b3a3d1c8b514cb
Standardrkd <pgj5927@gmail.com>
2026-02-11 18:42:49 +0900

    시간초과 ;;

--- 1072_게임_unsolved.py
+++ 1072_게임_unsolved.py
@@ -7,11 +7,11 @@ if X == Y:
     print(-1)
     exit(0)
 
-init = percent(X,Y)
+init = percent(X,Y) # 처음 확률 (소숫점은 버림)
 cnt = X // 2
 while True:
-    print(percent(X+cnt, Y+cnt))
-    if init == percent(X+cnt, Y+cnt):
+    # print(percent(X+cnt, Y+cnt))
+    if init == percent(X+cnt, Y+cnt): # 종료 조건
         while init == percent(X+cnt, Y+cnt):
             cnt += 1
         print(cnt)
@@ -19,3 +19,4 @@ while True:
 
     cnt = cnt//2
 
+# 27133 27002 -> 시간 초과 (X, Y가 같은 경우에만 불가능 한 것이 아님)
