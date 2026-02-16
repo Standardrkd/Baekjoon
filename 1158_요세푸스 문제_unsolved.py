0aa8f7809cff65fa73e4a50d53c6513b70994379
Standardrkd <pgj5927@gmail.com>
2026-02-16 17:50:01 +0900

    요세푸스를 다시 풂

--- 1158_요세푸스 문제_unsolved.py
+++ 1158_요세푸스 문제_unsolved.py
@@ -3,12 +3,10 @@ N, K = map(int, input().split())
 
 Josephus = [i+1 for i in range(N)] # 1,2,3,4 ...
 result = []
-index = 0
+index = K-1
+
 while Josephus:
-    result.append(Josephus.pop((index + K-1)%len(Josephus)))
-    if len(Josephus) != 0:
-        index = (index+K-1)%len(Josephus)
-        
-
-
+    result.append(Josephus.pop(index%len(Josephus)))
+    if Josephus:
+        index = (index + K -1)%(len(Josephus)) # 원소가 하나씩 없어지니까 index더할 때 1도 빼줘야함.
 print(result)
--- 1459_ 걷기.py
+++ 1459_ 걷기.py
@@ -1,5 +1,3 @@
 X, Y, W, S = map(int, input().split())
-good = False
-if S < W*2:
-    good = True
+
 
