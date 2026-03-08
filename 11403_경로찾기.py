a84af3a239b2d37933ca3f7c7ffaab908454612a
Standardrkd <pgj5927@gmail.com>
2026-03-08 14:13:39 +0900

    커밋

--- 11403_경로찾기.py
+++ 11403_경로찾기.py
@@ -2,6 +2,14 @@ from collections import deque
 
 N = int(input())
 
+grid = [list(map(int, input().split())) for _ in range(N)]
+graph = [[] for _ in range(N)]
+
+for i in range(N): # 그래프로 만들기
+    for j in range(N):
+        if grid[i][j] == 1:
+            graph[i].append(j)
+
 
 
 
