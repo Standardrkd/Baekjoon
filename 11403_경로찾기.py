1dec02b6b921b9375946362fe711124c7423804c
Standardrkd <pgj5927@gmail.com>
2026-03-15 13:26:19 +0900

    뭐야 커밋을 왜 지금해

--- 11403_경로찾기.py
+++ 11403_경로찾기.py
@@ -1,34 +1,27 @@
 from collections import deque
-
 N = int(input())
 q = deque()
-grid = [list(map(int, input().split())) for _ in range(N)] # 입력 행렬
-result = [[0] * N for _ in range(N)] # 결과 행렬
-graph = [[] for _ in range(N+1)] # 1인덱스 사용
-visited = [False] * (N+1)
-
-for i in range(N): # 그래프로 만들기
+g = [list(map(int, input().split())) for _ in range(N)]
+R = [[0] * N for _ in range(N)]
+G = [[] for _ in range(N+1)]
+for i in range(N):
     for j in range(N):
-        if grid[i][j] == 1:
-            graph[i+1].append(j+1)
-
-
+        if g[i][j] == 1:
+            G[i+1].append(j+1)
 def bfs(i):
     while q:
         x = q.popleft()
-        for nx in graph[x]:
-            if not visited[nx]:
-                result[i-1][nx-1] = 1
-                q.append(nx)
-                visited[nx] = True
-
+        for j in G[x]:
+            if not v[j]:
+                R[i-1][j-1] = 1
+                q.append(j)
+                v[j] = True
 for i in range(1,N+1):
-    visited = [False] * (N+1)
+    v = [False] * (N+1)
     q.append(i)
     bfs(i)
-    
-for row in result:
-    print(*row)
+for r in R:
+    print(*r)
 
 
 
